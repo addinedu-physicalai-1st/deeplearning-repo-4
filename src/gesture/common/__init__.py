da16572cd7477de28702ee287e9ffd199b8b878f
joey114132 <jwlee8403@gmail.com>
2026-01-28 18:48:34 +0900

    feat: game simulation

--- src/gesture/common/__init__.py
+++ src/gesture/common/__init__.py
@@ -5,12 +5,19 @@ Common 제스처 모듈
 
 # 제스처 클래스들을 import하여 자동 등록되도록 함
 from .start_gesture import StartGesture
+from .start_gesture import StartGesture
 from .stop_gesture import StopGesture
+from src.gesture.base.static_gesture import SpidermanGesture, PointUpGesture, PointDownGesture, PointLeftGesture, PointRightGesture
 
 # 레지스트리에 자동 등록
 from src.gesture.registry.gesture_registry import GestureRegistry
 registry = GestureRegistry()
 registry.register(StartGesture)
 registry.register(StopGesture)
+registry.register(SpidermanGesture)
+registry.register(PointUpGesture)
+registry.register(PointDownGesture)
+registry.register(PointLeftGesture)
+registry.register(PointRightGesture)
 
-__all__ = ['StartGesture', 'StopGesture']
+__all__ = ['StartGesture', 'StopGesture', 'SpidermanGesture', 'PointUpGesture', 'PointDownGesture', 'PointLeftGesture', 'PointRightGesture']
